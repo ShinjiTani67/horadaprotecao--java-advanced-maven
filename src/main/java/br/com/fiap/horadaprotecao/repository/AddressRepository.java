00d1a9046c0afc8fff78274dadc8016eb4ca2e04
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-27 09:45:04 -0300

    create camada repository

---
+++ src/main/java/br/com/fiap/horadaprotecao/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.horadaprotecao.repository;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public interface AddressRepository {
+}
---
+++ src/main/java/br/com/fiap/horadaprotecao/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.horadaprotecao.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+}
