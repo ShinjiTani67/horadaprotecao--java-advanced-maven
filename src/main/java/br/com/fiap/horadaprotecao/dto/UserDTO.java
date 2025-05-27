bf79ddc8f8085904f1cc57369ec689b62bdd863d
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-27 09:46:31 -0300

    create camada dto

---
+++ src/main/java/br/com/fiap/horadaprotecao/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package br.com.fiap.horadaprotecao.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+}
