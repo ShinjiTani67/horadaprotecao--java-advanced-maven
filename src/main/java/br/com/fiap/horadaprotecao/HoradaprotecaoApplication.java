96f94cd5f9f9496fdfaecc9543b875a9692ab23d
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-27 09:30:45 -0300

    create project

---
+++ src/main/java/br/com/fiap/horadaprotecao/HoradaprotecaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.horadaprotecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoradaprotecaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoradaprotecaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/horadaprotecao/HoradaprotecaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.horadaprotecao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoradaprotecaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
