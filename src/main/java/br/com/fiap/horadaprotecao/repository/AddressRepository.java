9be1f643ad3d1f6624e757fa74e83556a14489ca
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 11:43:01 -0300

    update camada repository

--- src/main/java/br/com/fiap/horadaprotecao/repository/AddressRepository.java
+++ src/main/java/br/com/fiap/horadaprotecao/repository/AddressRepository.java
@@ -1,7 +1,11 @@
 package br.com.fiap.horadaprotecao.repository;
 
+import br.com.fiap.horadaprotecao.entity.Address;
 import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
 
 @Entity
-public interface AddressRepository {
+public interface AddressRepository extends JpaRepository<UUID, Address> {
 }
--- src/main/java/br/com/fiap/horadaprotecao/repository/UserRepository.java
+++ src/main/java/br/com/fiap/horadaprotecao/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package br.com.fiap.horadaprotecao.repository;
 
+import br.com.fiap.horadaprotecao.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository <UUID, User> {
 }
