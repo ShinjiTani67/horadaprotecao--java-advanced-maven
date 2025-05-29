38422ddb9bb854073aaa691c5fc73916f5ced3aa
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 12:07:46 -0300

    update homecontroller

--- src/main/java/br/com/fiap/horadaprotecao/controller/HomeController.java
+++ src/main/java/br/com/fiap/horadaprotecao/controller/HomeController.java
@@ -1,4 +1,25 @@
 package br.com.fiap.horadaprotecao.controller;
 
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model, Authentication authentication) {
+        if (authentication != null) {
+            String username = authentication.getName();
+            model.addAttribute("username", username);
+        }
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:home";
+    }
 }
