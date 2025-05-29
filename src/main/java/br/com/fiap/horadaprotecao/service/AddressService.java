4cc0d3c47d1a854dc83619aa724f712921addc7b
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 15:50:56 -0300

    Update AddressService.java

--- src/main/java/br/com/fiap/horadaprotecao/service/AddressService.java
+++ src/main/java/br/com/fiap/horadaprotecao/service/AddressService.java
@@ -2,6 +2,55 @@ package br.com.fiap.horadaprotecao.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
+
+
+@Servic
+@AllArgsConstructor
 public class AddressService {
+
+  private final AddressRepository repository;
+
+  private AddressDTO convertToDTO(Address address){
+    AddressDTO dto = new AddressDTO();
+    //set e get
+    dto.setUuid(address.geUuid());
+    return dto;
+  }
+  private Adress convertToEntity(AddressDTO dto){
+    //set e get
+    Address address = new Address();
+    address.seUuid(dto.getUuid());
+  }
+
+  public AddressDTO save(AddressDTO addressdto){
+    Address address = convertoEntity(AddressDTO);
+
+    if (address.getUuid()==null  address.getUuid().isBlank()){
+       address.setUuid(UUI.randomUUID().ToString());
+      }
+     address = (Address) reposiory.save(address);
+    return convetToDTO(address);
+  }
+
+  //List<AddressDTO>
+  //deleteByUuid
+  //findByUuid
+
+  //public List<UserDTO> getUser(){
+  //      return repository.findAll().stream()
+  //              .map(this::convertToDTO)
+  //              .collect(Collectors.toList());
+  //  }
+
+    //public void deleteById(String id){
+      //  repository.deleteById(id);
+    //}
+
+    //public UserDTO findById(String id){
+      //  Optional<User> byId = repository.findById(id);
+        //if (byId.isPresent())
+          //  return convertToDTO(byId.get());{
+        //}
+        //throw new RuntimeException("Usuario com id" + id + "nao encontrado");
+    //}
 }
