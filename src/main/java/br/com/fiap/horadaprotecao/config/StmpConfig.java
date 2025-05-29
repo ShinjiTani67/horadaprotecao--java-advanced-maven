e1efd44d82a2f833b2d7b2fda5e16b637fcb52fd
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 12:15:05 -0300

    update stmpConfig

--- src/main/java/br/com/fiap/horadaprotecao/config/StmpConfig.java
+++ src/main/java/br/com/fiap/horadaprotecao/config/StmpConfig.java
@@ -17,8 +17,8 @@ public class StmpConfig {
     @Value("${spring.mail.port}")
     private int port;
 
-    //@Value("${spring.mail.properties.mail.stmp.auth}")
-    //private Boolean auth;
+    @Value("${spring.mail.properties.mail.stmp.auth}")
+    private Boolean auth;
 
     @Bean
     public JavaMailSender getJavaMailSender() {
