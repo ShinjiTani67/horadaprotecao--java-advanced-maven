500f8cbd4f507a7c4801c978ba38cb2a57d24f61
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 16:52:22 -0300

    Update FloodZoneService.java

--- src/main/java/br/com/fiap/horadaprotecao/service/FloodZoneService.java
+++ src/main/java/br/com/fiap/horadaprotecao/service/FloodZoneService.java
@@ -1,4 +1,40 @@
 package br.com.fiap.horadaprotecao.service;
 
+@Service
+@AllArgsConstructor
 public class FloodZoneService {
+  private final FloodedZoneRepository repository;
+
+  private FloodedZoneDTO convertToDTO (FloodedZone floodzone){
+    FloodedZoneDTO dto = new FloodedZoneDTO();
+    dto,setUuid(floodedzone.getUuid());
+    //get e set
+    return dto;
+  }
+
+  private FloodedZone convertToEntity(FloodedZoneDTO dto){
+
+    FloodedZone floodedzone = new FloodedZone();
+    floodedzone.setUuid(dto.getUuid());
+    //set e get
+  }
+  public FloodedZoneDTO save(FloodedZoneDTO floodedzonedto){
+    
+  }
+  public List<FloodedZoneDTO> getFloodedZone(){
+      return repository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+  }
+  public void deleteById(String uuid){
+        repository.deleteUuid(uuid);
+    }
+
+  public FloodedZoneDTO findByUuid(Uuid uuid){
+    Optional<FloodedZone> byUuid = repository.findbyUuid(uuid);
+    if (byUuid.isPresent())
+            return convertToDTO(byUuid.get());{
+        }
+        throw new RuntimeException("Usuario com id" + id + "nao encontrado");
+  }
 }
