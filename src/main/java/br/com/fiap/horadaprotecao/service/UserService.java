8c281545e7cffcd7665f9d3d4825ff287dc6e9b1
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 15:35:00 -0300

    Update UserService.java

--- src/main/java/br/com/fiap/horadaprotecao/service/UserService.java
+++ src/main/java/br/com/fiap/horadaprotecao/service/UserService.java
@@ -2,7 +2,53 @@ package br.com.fiap.horadaprotecao.service;
 
 import org.springframework.stereotype.Service;
 
+
+//trocar id por uuid
+
+
 @Service
+@AllArgsConstructor
 public class UserService {
-    
+    private final Useepository repository;
+
+    private UserDto convertToDTO (User user){
+        UserDTO dto = new UserDTO();
+        //set e gets
+        return dto;
+    }
+
+    private User convertToEntity(UserDTO dto){
+        User user = new User();
+        //set e gets
+        return user;
+    }
+
+    public UserDTO save(UserDTO userDTO){
+        User user = convertToEntity(userDTO);
+
+        if (user.getId() == null || user.getId().isBlank()) {
+            user.setId(UUID.randomUUID().toString());
+        }
+
+        user = (User) repository.save(user);
+        return convertToDTO(user);
+    }
+
+    public List<UserDTO> getUser(){
+        return repository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteById(String id){
+        repository.deleteById(id);
+    }
+
+    public UserDTO findById(String id){
+        Optional<User> byId = repository.findById(id);
+        if (byId.isPresent())
+            return convertToDTO(byId.get());{
+        }
+        throw new RuntimeException("Usuario com id" + id + "nao encontrado");
+    }
 }
