d26548988a7e83731ccffcbaf5f77fdb6ed56314
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 12:06:38 -0300

    update errorcontroller

--- src/main/java/br/com/fiap/horadaprotecao/controller/ErrorController.java
+++ src/main/java/br/com/fiap/horadaprotecao/controller/ErrorController.java
@@ -1,4 +1,26 @@
 package br.com.fiap.horadaprotecao.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
 public class ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object errorMessage = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+
+        model.addAttribute("status", statusCode);
+        model.addAttribute("message", errorMessage != null ? errorMessage : "Erro inesperado");
+
+        return "error";
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
 }
