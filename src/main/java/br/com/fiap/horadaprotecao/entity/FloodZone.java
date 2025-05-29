d48c8613bd7392c1c16dcaa6b18856c4beec998b
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-29 11:54:57 -0300

    create entity floodzone

---
+++ src/main/java/br/com/fiap/horadaprotecao/entity/FloodZone.java
@@ -0,0 +1,22 @@
+package br.com.fiap.horadaprotecao.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import java.util.UUID;
+
+@Data
+@Entity
+@ToString
+@Table(name = "tb_floodzone")
+public class FloodZone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID uuid;
+
+    private String nivelRisco;
+
+    private Double raioEmKm;
+
+}
