99f73612baabcb754a0bbd56ff1aea048742448d
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-01 22:01:52 -0300

    update geral

--- src/main/java/br/com/fiap/horadaprotecao/config/InternationalizationConfig.java
+++ src/main/java/br/com/fiap/horadaprotecao/config/InternationalizationConfig.java
@@ -1,5 +1,4 @@
-package fiap.com.br.demo.config;
-
+package br.com.fiap.horadaprotecao.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
