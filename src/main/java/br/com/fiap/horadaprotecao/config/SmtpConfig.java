a455570848a24c427fb4911f1fc3ae3fd206d8d3
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-02 08:51:48 -0300

    update smtpconfig

--- src/main/java/br/com/fiap/horadaprotecao/config/SmtpConfig.java
+++ src/main/java/br/com/fiap/horadaprotecao/config/SmtpConfig.java
@@ -1,12 +1,10 @@
 package br.com.fiap.horadaprotecao.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
 
 import java.util.Properties;
 
@@ -19,20 +17,30 @@ public class SmtpConfig {
     @Value("${spring.mail.port}")
     private int port;
 
-    @Value("${spring.mail.properties.mail.stmp.auth}")
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
     private Boolean auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private Boolean startTls;
 
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
         mailSender.setHost(host);
         mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", false);
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", startTls);
 
         return mailSender;
-
     }
 }
