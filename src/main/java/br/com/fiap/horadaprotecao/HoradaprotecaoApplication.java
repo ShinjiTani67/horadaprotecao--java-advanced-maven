83eb7c70701246f2c1affca18d108e6c882a4557
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-02 08:21:43 -0300

    update geral

--- src/main/java/br/com/fiap/horadaprotecao/HoradaprotecaoApplication.java
+++ src/main/java/br/com/fiap/horadaprotecao/HoradaprotecaoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HoradaprotecaoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HoradaprotecaoApplication.class, args);
 	}
-
 }
