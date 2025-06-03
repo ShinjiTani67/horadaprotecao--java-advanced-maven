0ac5432fde7c74bd65c6d2c82a93160fcf2b5cfe
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-03 08:21:42 -0300

    update de sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,24 @@
-INSERT INTO user (id, username, password, nome, cpf, telefone, email)
-VALUES
-  ('8e5379d4-daf6-4ed7-a934-72a7f8e31ef1', 'admin', '$2a$10$2b2b2b2b2b2b2b2b2b2bOeMXzWcIm8g0aZbF2fZ5cq9CayDmSbKjK', 'Administrador', '00000000000', '11999999999', 'admin@teste.com'),
+DROP TABLE IF EXISTS users;
 
-  ('22222222-2222-2222-2222-222222222222', 'usuario', '$2a$10$2b2b2b2b2b2b2b2b2b2bOeMXzWcIm8g0aZbF2fZ5cq9CayDmSbKjK', 'Usuário Comum', '11111111111', '11888888888', 'usuario@teste.com');
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+-- ADMIN (senha: admin)
+INSERT INTO users (id, username, password, role) VALUES (
+    '8e5379d4-daf6-4ed7-a934-72a7f8e31ef1',
+    '8e5379d4-daf6-4ed7-a934-72a7f8e31ef1',
+    '$2a$10$EVKHKJKoPC6vChV2FLHp4OB14eDWHxwW7F3zp8XxZQfZIRjPajOYm', -- "admin"
+    'ROLE_ADMIN'
+);
+
+-- USER (senha: fernando)
+INSERT INTO users (id, username, password, role) VALUES (
+    '6e4e19b1-a5f0-4d49-9b75-26b66c9427e2',
+    'fernando@email.com',
+    '$2a$10$W8QoCSa4ZJGEOmfVQFeU.eWmMz1zPu4OibNUHGl5Xh.pNq0cN5IVK', -- "fernando"
+    'ROLE_USER'
+);
