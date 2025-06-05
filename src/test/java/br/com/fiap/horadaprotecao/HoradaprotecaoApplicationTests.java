ab4739b6f02424b31cf6855cc62c7ad39c543bf6
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-05 09:58:08 -0300

    update correção de ambiente de teste

--- src/test/java/br/com/fiap/horadaprotecao/HoradaprotecaoApplicationTests.java
+++ src/test/java/br/com/fiap/horadaprotecao/HoradaprotecaoApplicationTests.java
@@ -1,8 +1,10 @@
 package br.com.fiap.horadaprotecao;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
