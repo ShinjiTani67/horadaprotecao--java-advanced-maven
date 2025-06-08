d5535dbb857c4effe937c990d276c9557832cb68
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-08 17:14:18 -0300

    update dashboardcontroller

--- src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
+++ src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
@@ -1,11 +1,17 @@
 package br.com.fiap.horadaprotecao.controller;
 
+import br.com.fiap.horadaprotecao.dto.AddressDTO;
+import br.com.fiap.horadaprotecao.dto.FloodZoneDTO;
 import br.com.fiap.horadaprotecao.dto.UserDTO;
+import br.com.fiap.horadaprotecao.service.AddressService;
+import br.com.fiap.horadaprotecao.service.FloodZoneService;
+import br.com.fiap.horadaprotecao.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @AllArgsConstructor
