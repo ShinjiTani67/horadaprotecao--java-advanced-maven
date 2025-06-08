f8de4c2bc98f1a48c96b576cbdd8953963b46a10
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-08 17:18:43 -0300

    update

--- src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
+++ src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
