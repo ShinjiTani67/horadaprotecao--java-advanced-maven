7c69ed75020d0ad80a77aef437bf8262b5b5c8b8
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-08 19:53:09 -0300

    update dashboard

--- src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
+++ src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
@@ -1,4 +1,4 @@
-1package br.com.fiap.horadaprotecao.controller;
+package br.com.fiap.horadaprotecao.controller;
 
 import br.com.fiap.horadaprotecao.dto.AddressDTO;
 import br.com.fiap.horadaprotecao.dto.FloodZoneDTO;
@@ -17,23 +17,23 @@ import java.util.List;
 @AllArgsConstructor
 public class DashboardController {
 
-    private final UserService userService;
-    private final AddressService addressService;
-    private final FloodZoneService floodZoneService;
+ private final UserService userService;
+ private final AddressService addressService;
+ private final FloodZoneService floodZoneService;
 
-    @GetMapping("/dashboard")
-    public String dashboard(Model model, Authentication authentication) {
-        String email = authentication.getName();
+ @GetMapping("/dashboard")
+ public String dashboard(Model model, Authentication authentication) {
+ String email = authentication.getName();
 
-        UserDTO user = userService.findByEmail(email);
-        model.addAttribute("user", user);
+ UserDTO user = userService.findByEmail(email);
+ model.addAttribute("user", user);
 
-        AddressDTO address = addressService.findByUserEmail(email);
-        model.addAttribute("address", address);
+ AddressDTO address = addressService.findByEmail(email);
+ model.addAttribute("address", address);
 
-        List<FloodZoneDTO> floodZones = floodZoneService.getFloodedZone();
-        model.addAttribute("floodZones", floodZones);
+ List<FloodZoneDTO> floodZones = floodZoneService.getFloodedZone();
+ model.addAttribute("floodZones", floodZones);
 
-        return "dashboard";
-    }
+ return "dashboard";
+ }
 }
