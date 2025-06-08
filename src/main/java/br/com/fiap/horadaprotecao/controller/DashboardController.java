c1c9a76c6ea0ef798c3b254100d9ce1309e7ac16
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-06-08 18:30:57 -0300

    Update DashboardController.java

--- src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
+++ src/main/java/br/com/fiap/horadaprotecao/controller/DashboardController.java
@@ -17,22 +17,23 @@ import java.util.List;
 @AllArgsConstructor
 public class DashboardController {
 
-    private final UserService userService;
-    private final AddressService addressService;
-    private final FloodZoneService floodZoneService;
+    private final UserService userService;
+    private final AddressService addressService;
+    private final FloodZoneService floodZoneService;
 
-    @GetMapping("/dashboard")
-    public String dashboard(Model model, Authentication authentication) {
-        String email = authentication.getName();
-        UserRepository user = userService.findById(string);
-        model.addAttribute("user", user);
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Authentication authentication) {
+        String email = authentication.getName();
 
-        AddressRepository address = addressService.findById(string);
-        model.addAttribute("address", address);
+        UserDTO user = userService.findByEmail(email);
+        model.addAttribute("user", user);
 
-        List<FloodZoneDTO> floodZones = floodZoneService.getFloodedZone();
-        model.addAttribute("floodZones", floodZones);
+        AddressDTO address = addressService.findByUserEmail(email);
+        model.addAttribute("address", address);
 
-        return "dashboard"; // ou "dashboard.html", se você quiser renomear a view
-    }
+        List<FloodZoneDTO> floodZones = floodZoneService.getFloodedZone();
+        model.addAttribute("floodZones", floodZones);
+
+        return "dashboard";
+    }
 }
